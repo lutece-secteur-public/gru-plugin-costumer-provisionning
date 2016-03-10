bb7670547ee29ed13ad62702c542745a585064c5
abdourahmane fall <abdourahmane.fall@atos.net>
2016-03-10 17:42:52 +0100

    uniformise costumer

--- src/java/fr/paris/lutece/plugins/costumerprovisionning/services/ProvisionningService.java
+++ src/java/fr/paris/lutece/plugins/costumerprovisionning/services/ProvisionningService.java
@@ -10,66 +10,10 @@ public class ProvisionningService {
 
 	
 	
-	public static Customer processGuidCuidTicketing(String strGuidFromTicket, String strCidFromTicket, UserDTO userDto)
-        {
-            
-             fr.paris.lutece.plugins.costumerprovisionning.business.UserDTO userDtoOpenAm=null; 
-             fr.paris.lutece.plugins.gru.business.customer.Customer gruCustomer = null;
-        
-             
-             if ( StringUtils.isEmpty( strCidFromTicket ) )
-        {
-            if ( !StringUtils.isEmpty( strGuidFromTicket ) )
-            {
-                //if guid is provided => we try to retrieve linked customer
-                gruCustomer = getCustomerByGuid( strGuidFromTicket );
-                userDtoOpenAm = UserInfoService.instance(  ).getUserInfo( strGuidFromTicket );
-            }
-
-            if ( gruCustomer == null )
-            {
-                //customer is unknown / not found => we create it
-                if ( userDtoOpenAm == null )
-                {
-                    userDtoOpenAm = userDto;
-                }
-                gruCustomer = createCustomerByGuid(strGuidFromTicket);
-                //create customer
-              //  gruCustomer = CustomerService.instance(  ).createCustomer( buildCustomer( userDto, strGuidFromTicket ) );
-                AppLogService.info( "New user created the guid : <" + strGuidFromTicket + "> its customer id is : <" +
-                    gruCustomer.getId(  ) + ">" );
-            }
-
-            //update CID
-//            ticket.setCustomerId( String.valueOf( gruCustomer.getId(  ) ) );
-//            TicketHome.update( ticket );
-        }  else
-        {
-            if ( StringUtils.isEmpty( strGuidFromTicket ) )
-            {
-                if ( StringUtils.isNumeric( strCidFromTicket ) )
-                {
-                    // CASE : cid but no guid:  find customer info in GRU database => try to retrieve guid from customer
-                   // gruCustomer = CustomerService.instance(  ).getCustomerByCid( strCidFromTicket );
-                    gruCustomer =getCustomerByCuid(strCidFromTicket);
-                }
-                else
-                {
-                    AppLogService.error( "Provided customerId is not numeric: <" + strCidFromTicket + ">" );
-                }
-
-                if ( gruCustomer == null )
-                {
-                     AppLogService.info( "No guid found for user cid : <" + strCidFromTicket + ">" );
-                }
-              
-            }
-        }
-          
-            return gruCustomer;
-        }
-	public static Customer processGuidCuid(String strGuid, String strCuid)
+	public static Customer processGuidCuid(String strGuid, String strCuid,UserDTO userDto)
 	{
+		
+		
 		
 		Customer gruCustomer=null;
 		
@@ -77,13 +21,13 @@ public class ProvisionningService {
         if ( StringUtils.isEmpty( strCuid ) )
         {
             // CASE 1.1 : no cid and no guid:  break the flux and wait for a new flux with one of them
-            if ( StringUtils.isEmpty( strGuid ) )
+            if ( StringUtils.isEmpty( strGuid ) && userDto == null)
             {
-              
+      
                 AppLogService.error( "Provionning - Error : JSON doesnot contains any GUID nor Customer ID : " + strCuid  );
             //    return error( "grusupply - Error : JSON doesnot contains any GUID nor Customer ID" );
             } // CASE 1.2  : no cid and guid:  look for a mapping beween an existing guid
-            else
+            else if(userDto==null && !StringUtils.isEmpty( strGuid ))
             {
             	
             	
@@ -100,6 +44,10 @@ public class ProvisionningService {
                     
                     AppLogService.info( "Provionning - New user created into the GRU for the guid : " + strGuid +" its customer id is : " + gruCustomer.getId(  ) );
                 }
+            }else 
+            {
+            	
+            	gruCustomer = createCustomerByGuid(userDto,strGuid);
             }
         } // CASE 2 : cid and (guid or no guid):  find customer info in GRU database
         else
@@ -127,12 +75,22 @@ public class ProvisionningService {
 		return  grusupplyCustomer;
 	}
 	
+	
 	/** Create costumer by Guid
 	 * */
 	public static  Customer createCustomerByGuid(String strGuid)
 	{		
 		UserDTO user = UserInfoService.instance(  ).getUserInfo( strGuid );	
 		Customer costumer=CustomerService.instance(  ).createCustomer( buildCustomer( user, strGuid ) );	
+		return  costumer;
+	}
+	
+	/** Create costumer by Guid
+	 * */
+	public static  Customer createCustomerByGuid(UserDTO userDto, String strGuidFromTicket)
+	{		
+		
+		Customer costumer=CustomerService.instance(  ).createCustomer( buildCustomer( userDto, strGuidFromTicket ) );	
 		return  costumer;
 	}
 	
